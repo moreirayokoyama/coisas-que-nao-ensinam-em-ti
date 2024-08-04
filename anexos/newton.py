2fdbd63a4e53be9cce94d931662ac03b870a61e0
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-08-04 12:23:10 -0300

    Deployed fcd734c with MkDocs version: 1.6.0

--- anexos/newton.py
+++ anexos/newton.py
@@ -1,14 +1,6 @@
 # Implementação do método de Newton para calcular raiz quadrada baseada no
 # livro _Structure and Interpretation of Computer Programs
 
-def good_enough(guess, x):
-    return abs((guess * guess) - x) < 0.001
-
-def average(x, y):
-    return (x + y) / 2
-
-def improve(guess, x):
-    return average(guess, x / guess)
 
 def sqrt_iter(guess, x):
     if good_enough(guess, x):
@@ -16,7 +8,20 @@ def sqrt_iter(guess, x):
     else:
         return sqrt_iter(improve(guess, x), x)
 
+def good_enough(guess, x):
+    return abs((guess * guess) - x) < 0.001
+
+
+def improve(guess, x):
+    return average(guess, x / guess)
+
+
+def average(x, y):
+    return (x + y) / 2
+    
+
 def sqrt(x):
     return sqrt_iter(1.0, x)
 
-print(sqrt(2))
+
+print(sqrt(2.0))
