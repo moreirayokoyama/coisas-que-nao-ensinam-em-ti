aeb4628a80e65d1af95db0f0292afdd232d4a3ec
Daniel Moreira Yokoyama <792153+moreirayokoyama@users.noreply.github.com>
2024-08-05 10:41:36 -0300

    Deployed ce8505e with MkDocs version: 1.6.0

--- anexos/newton.py
+++ anexos/newton.py
@@ -6,7 +6,10 @@ def sqrt_iter(guess, x):
     if good_enough(guess, x):
         return guess
     else:
-        return sqrt_iter(improve(guess, x), x)
+        return sqrt_iter(
+            improve(guess, x), x
+        )  # chamada recursiva à função, usando palpites melhorados
+
 
 def good_enough(guess, x):
     return abs((guess * guess) - x) < 0.001
@@ -18,10 +21,11 @@ def improve(guess, x):
 
 def average(x, y):
     return (x + y) / 2
-    
 
+
+# Abstrai a chamada da função sqrt_iter
 def sqrt(x):
-    return sqrt_iter(1.0, x)
+    return sqrt_iter(1.0, x)  # usamos 1.0 como palpite inicial da raiz quadrada de x
 
 
 print(sqrt(2.0))
